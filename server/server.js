7bb8fdc4125b29239e4c509bc02931b4e59f92a5
atharva <atharva@truscholar.io>
2022-08-21 16:25:39 +0530

    added comments functionality for testing

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 import express from 'express';
-
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+const comments = {
+  'learn-react': {
+    comments: []
+  },
+  'learn-node': {
+    comments: []
+  }
+};
 
 app.use(express.json({ extended: true }))
 
@@ -17,6 +25,14 @@ app.post('/', (req, res) => {
 
 app.get('/greet/:name', (req, res) => {
   res.send(`Hellot hre bori ${req.params.name}`);
+});
+
+app.post('/api/articles/addcomment/:name', (req, res) => {
+  const { username, text } = req.body;
+  const article = req.params.name;
+
+  comments[article].comments.push({username, text});
+  res.send(comments[article]);
 });
 
 app.listen(PORT, () => {
